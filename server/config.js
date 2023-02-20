f39c20061ecea87d148faa02cfde8c106b2c0f8a
kritsanapr <kritsana.pr.41@gmail.com>
2023-02-20 21:04:27 +0700

    Add server and develop function auth

---
+++ server/config.js
@@ -0,0 +1,6 @@
+
+// const DATABASE = "mongodb+srv://dbuser:dbpassword@your_db_name.pbn7j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const DATABASE = "mongodb://localhost:27017/dbname";
+const JWT_SECRET = "some_secret_letters_numbers";
+const SENDGRID_KEY = "SG.your.secret-key";
+const EMAIL_FROM = "yourname@gmail.com";
