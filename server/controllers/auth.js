f7d6134c2a216885d9a19b0563a0b5ed36730c20
kritsanapr <kritsana.pr.41@gmail.com>
2023-06-10 00:05:29 +0700

    feat : function  api resetPassword and forgotPassword

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -78,3 +78,69 @@ export const signin = async (req, res) => {
     console.log(err);
   }
 };
+
+export const forgotPassword = async (req, res) => {
+  const { email } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.json({
+      error: "User not found",
+    });
+  }
+
+  // Generate code
+  const resetCode = nanoid(5).toUpperCase();
+  // Save to database
+  user.resetCode = resetCode;
+  user.save();
+
+  // Prepare for email
+  const emailData = {
+    from: process.env.EMAIL_FROM,
+    to: user.email,
+    subject: `Password reset link`,
+    html: `
+        <h1>Please use the following code to reset your password</h1>
+        <h2>${resetCode}</h2>
+        <hr />
+        <p>This email may contain sensitive information</p>
+        <p>https://kolaraat.com</p>
+    `,
+  };
+
+  try {
+    await sgMail.send(emailData);
+    console.log(data);
+    return res.json({ ok: true });
+  } catch (err) {
+    console.log(err);
+    return res.json({
+      error: "Error sending email",
+    });
+  }
+};
+
+export const resetPassword = async (req, res) => {
+  try {
+    const { email, password, resetCode } = req.body;
+    // FInd user based on email and resetCode
+    const user = await User.findOne({ email, resetCode });
+    if (!user) {
+      return res.json({
+        error: "Invalid credentials",
+      });
+    }
+
+    if (!password || password.length < 6) {
+      return res.json({
+        error: "Password is required and should be 6 characters long",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.json({
+      error: "Error resetting password",
+    });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ mongoose
   .then(() => console.log("DB connected"))
   .catch((err) => console.log("DB CONNECTION ERROR: ", err));
 
-app.use(express.json());
+app.use(express.json({ limit: "4mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
